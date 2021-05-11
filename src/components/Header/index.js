7e84e1361438bdde360e5cba332f4de1b3000ba3
damian.deleon <damianmdeleon@gmail.com>
2021-05-11 11:16:57 -0500

    rendered first component- Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return (
+    <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Click on each heding to filter or use the serach box to narrow your results</p>
+    </div>
+    )
+}
+
+export default Header;
